b35525c707c71d01242ecb6988158ca37dfc8e53
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2024-04-10 00:12:24 -0300

    Add files via upload

---
+++ Atividade Chat Com Js/src/js/chat(index).js
@@ -0,0 +1,39 @@
+// Criar função de enviar a mensagem
+function sendMessage() {
+    // document.querySelector("#user-input"): Seleciona o elemento HTML que possui o 
+    // ID "user-input".
+    let userInput = document.querySelector("#user-input").value; // .value: Acessa op valor 
+                                                                 // desse elemento, que 
+                                                                 // contém o valor inserido pelo 
+                                                                 // usuário em um campo de 
+                                                                 // entrada HTML.
+    // document.querySelector("#chat-container"): Seleciona o elemento HTML que possui 
+    // o ID "chat-container".
+    let chatContainer = document.querySelector("#chat-container");
+    // Essa linha cria um novo elemento HTML <div>
+    let newMessage = document.createElement("div");
+    // Vai adicionar a estilização da class (client-message) na div
+    newMessage.classList.add("client-message");
+    
+    // Criando um elemento H4
+    let messageHeader = document.createElement("h4");
+    // Adicionando um valor ao elemento H4 criado a cima
+    messageHeader.textContent = "Cliente";
+    // Coloca o elemento H4 criado com filho na div criada na linha 14
+    newMessage.appendChild(messageHeader);
+    
+    // Criando um elemento P (parágrafo)
+    let messageContent = document.createElement("p");
+    // Vai inseiri o valor dentro do elemento p, igual o valor da variável
+    // userInput (linha 5), onde seria o que o usuário digitou
+    messageContent.textContent = userInput;
+    // Coloca o elemento p criado com filho na div criada na linha 14
+    newMessage.appendChild(messageContent);
+    
+    // Aqui irá fazer que a nova mensagem, vire o filho da div pai (chatContainer)
+    chatContainer.appendChild(newMessage);
+    // Assim que o usuário envia a mensagem, o campo input é limpo
+    document.querySelector("user-input").value = "";
+  }
+
+  
